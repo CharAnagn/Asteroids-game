c24be7ef3265ba71fb20942383ce4f7c77cd953d
Babis Anagnostou <cganagnostou@gmail.com>
2025-08-03 10:49:47 +0200

    Add frames

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ from constants import *
 def main():
     pygame.init()
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = game_clock.tick(60) / 1000
 
         screen.fill("black")
 
